68235c1335723e53051d8b2aba47be4640b45981
Tomas Bjerre <tomasbjerre@yahoo.com>
2013-07-03 16:23:13 +0200

    Adding tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -8,7 +8,7 @@ import io
 class TestPythonSearch(unittest.TestCase):
 
     def setUp(self):
-        self.ps = PythonSearch('../../resources/stopwords.txt', '../../resources/stopwords_prepare.txt', '../../resources/rules.txt', '../../resources/rules_prepare.txt')
+        self.ps = PythonSearch('../resources/stopwords.txt', '../resources/stopwords_prepare.txt', '../resources/rules.txt', '../resources/rules_prepare.txt')
         self.maxDiff = None
 
     def __test_keys(self, input_file, output_file):
@@ -17,16 +17,16 @@ class TestPythonSearch(unittest.TestCase):
         self.assertEqual(" ".join(self.ps.get_keys(input)).strip(), output.strip())
 
     def test_input1(self):
-        self.__test_keys("../../test/1_input.txt", "../../test/1_output.txt")
+        self.__test_keys("1_input.txt", "1_output.txt")
     
     def test_input2(self):
-        self.__test_keys("../../test/2_input.txt", "../../test/2_output.txt")
+        self.__test_keys("2_input.txt", "2_output.txt")
     
     def test_input3(self):
-        self.__test_keys("../../test/3_input.txt", "../../test/3_output.txt")
+        self.__test_keys("3_input.txt", "3_output.txt")
     
     def test_input4(self):
-        self.__test_keys("../../test/4_input.txt", "../../test/4_output.txt")
+        self.__test_keys("4_input.txt", "4_output.txt")
 
     def test_string1(self):
         self.assertEqual("fung gubbe skyland sluta", " ".join(self.ps.get_keys("skylanders gubbe sluta fungera")).strip())
